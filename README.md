31e8c4aa4637e332c17674e5f5ede6b353d8924c
prazd <alexokda@list.ru>
2019-01-14 02:18:27 +0300

    add etc and eth

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # devops-nodes
 Guide how to set up your full node for BTC/BTH/ETH/ETC/LTC
+
+ - ETH(Parity)
+
+Fast start:
+```
+# sudo docker run --restart=always -ti -d -p 8545:8545 \ 
+-p 8546:8546 -p 30303:30303  -p 30303:30303/udp \ 
+parity/parity:v2.1.3 --ui-interface all --jsonrpc-interface all 
+```
+Full start(with docker volume) - eth.sh
+
+- ETC(Parity)
+
+```
+# sudo  docker run --restart=always -ti -d -p 8545:8545 \ 
+-p 8546:8546 -p 30303:30303 -p 30303:30303/udp \ 
+parity/parity:v2.1.3 --ui-interface all --jsonrpc-interface all \
+ --chain classic 
+```
+Full start(with docker volume) - etc.sh
+
---
+++ etc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+docker pull parity/parity:nightly
+docker rm -f parity
+docker run --restart always -ti -d \
+-p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
+--name parity --restart always \
+-v /srv/parity:/root/.local/share/io.parity.ethereum/ parity/parity:v2.1.3 \
+--jsonrpc-interface '0.0.0.0' --jsonrpc-hosts="all" \
+--author 0xYOURETHADDRESSHERE \
+--auto-update="all" \
+--stratum --stratum-interface=0.0.0.0 \
+--jsonrpc-cors null \
+--unsafe-expose \
+--db-compaction hdd \
+--mode active --cache-size 4096 \
+--ui-interface 0.0.0.0 --ui-no-validation \
+--chain classic
+docker logs --follow parity
