cf5efe8dba057a709bb6e8ddf942fbd8640cd5cc
prazd <alexokda@list.ru>
2019-02-02 17:38:15 +0300

    refactor

--- eth_etc.sh
+++ eth_etc.sh
@@ -29,7 +29,7 @@ cp scripts/docker/alpine/Dockerfile .
 docker build -t $ConteinerName .
 
 
-# docker run
+
 docker run --restart always -ti -d \
 -p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
 --name parity --restart always \
