8a9ea23d2e137e51526debc5f38e295da178735d
prazd <alexokda@list.ru>
2019-02-03 16:46:44 +0300

    refactor

--- setup_and_run/main.py
+++ setup_and_run/main.py
@@ -9,10 +9,8 @@ from os import system
 @click.option('--curr', prompt="Cryptocurrency", help='Cryptocurrency\nExample: --curr=eth')
 def run(key,user,addr,curr):
     """Simple program for nodes."""
-    if curr == "eth" or curr == "etc":
-        script = "eth_etc.sh"
-    else:
-        script = f"{curr}.sh"
+
+    script = f"{curr}.sh"
 
     system(f'''ssh -i keys/\"{key}\" {user}@{addr} \"
     sudo apt-get update && sudo apt-get upgrade;
