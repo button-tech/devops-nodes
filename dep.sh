92d704bd36794efeb150e5afd4a7ea2519c27cd6
Alex Prazdnikov <34573846+prazd@users.noreply.github.com>
2019-03-03 20:56:33 +0300

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -17,7 +17,6 @@ apt update
 apt upgrade
 apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt install docker-ce
