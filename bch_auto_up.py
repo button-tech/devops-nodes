56c7cce9ba6c2c06d3226eca51e77393e5454939
prazd <alexokda@list.ru>
2019-03-08 21:05:28 +0100

    bch auto up script

---
+++ bch_auto_up.py
@@ -0,0 +1,25 @@
+from os import system
+import socket
+import time
+
+def isOpen(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result =  sock.connect_ex(('127.0.0.1', int(port)))
+    sock.close()
+    return result
+
+def bchNodeUp():
+      system("cd ~/bitprim-insight/bitprim.insight;nohup dotnet bin/Release/netcoreapp2.0/bitprim.insight.dll --server.port=3000 --server.address=0.0.0.0 &")
+
+def bchUpCheck():
+     result = isOpen(3000)
+     if result == 0:
+         pass
+     else:
+         print("was down")
+         print(time.time())
+         BchNodeUp()
+
+while True:
+    bchUpCheck()
+    time.sleep(10)
