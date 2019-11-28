f77a29ff41b32ec7c582fc46b7cb5f775d02a711
Alexey Prazdnikov <34573846+prazd@users.noreply.github.com>
2019-11-28 16:08:56 +0300

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -12,8 +12,6 @@ apt install git
 apt install tmux
 
 # install docker
-apt update
-apt upgrade
 apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
